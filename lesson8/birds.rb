7150c67b453cb5179f3971c2eb86ac14f9ce3c3e
DimaYakimenko <yakimenkodims@gmail.com>
2022-05-26 10:56:19 +0300

    fix

--- lesson8/birds.rb
+++ lesson8/birds.rb
@@ -1,38 +1,21 @@
-argument0 = ARGV[0]
-argument1 = ARGV[1]
+temperature = ARGV[0]
+season = ARGV[1]
 
-if argument0.to_i >= 22 && argument0.to_i <= 30
+if temperature.to_i >= 22 && temperature.to_i <= 30 && season == nil
   abort 'Скорее идите в парк, соловьи поют!'
-
-elsif argument0.to_i >= 15 && argument0.to_i <= 35 && argument1.to_i == 1
-  abort 'Скорее идите в парк, соловьи поют!'
-
-elsif argument0 == nil
-puts 'Какая сейчас температура?'
-user_choice1 = gets.chomp
-puts 'Какое время года? (0 - весна, 1 - лето, 2 - осень, 3 - зима)?'
-user_choice2 = gets.chomp
-
-  if user_choice1.to_i >= 22 && user_choice1.to_i <= 30
-  abort 'Скорее идите в парк, соловьи поют!'
-  elsif user_choice1.to_i >= 15 && user_choice1.to_i <= 35 && user_choice2.to_i == 1
-  abort 'Скорее идите в парк, соловьи поют!'
-  else
-  abort 'Сейчас соловьи молчат, греются или прохлаждаются :)'
-  end
-
-else
-  abort 'Сейчас соловьи молчат, греются или прохлаждаются :)'
+elsif temperature.nil?
+  puts 'Какая сейчас температура?'
+  temperature = STDIN.gets.chomp
 end
 
-puts 'Какая сейчас температура?'
-user_choice1 = gets.chomp
-puts 'Какое время года? (0 - весна, 1 - лето, 2 - осень, 3 - зима)?'
-user_choice2 = gets.chomp
+if season.nil?
+  puts 'Какое время года? (0 - весна, 1 - лето, 2 - осень, 3 - зима)?'
+  season = STDIN.gets.chomp
+end
 
-if user_choice1.to_i >= 22 && user_choice1.to_i <= 30
+if temperature.to_i >= 22 && temperature.to_i <= 30
   abort 'Скорее идите в парк, соловьи поют!'
-elsif user_choice1.to_i >= 15 && user_choice1.to_i <= 35 && user_choice2.to_i == 1
+elsif temperature.to_i >= 15 && temperature.to_i <= 35 && season.to_i == 1
   abort 'Скорее идите в парк, соловьи поют!'
 else
   abort 'Сейчас соловьи молчат, греются или прохлаждаются :)'
