76f71aba9be587ab262e96260b1f59afe7fa71da
DimaYakimenko <yakimenkodims@gmail.com>
2022-06-08 21:15:57 +0300

    add chameleon

---
+++ lesson11/chameleon.rb
@@ -0,0 +1,5 @@
+require './chameleonclass.rb'
+cham = Chameleo.new
+cham.col
+cham.col
+cham.col
---
+++ lesson11/chameleonclass.rb
@@ -0,0 +1,6 @@
+class Chameleo
+  def col
+  color = gets.chomp.to_s
+  puts "Теперь я #{color}"
+  end
+end
