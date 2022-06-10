3310b60acf04eab1aeea7bdcddcf19edfe877554
DimaYakimenko <yakimenkodims@gmail.com>
2022-06-10 12:25:22 +0300

    exercise

---
+++ lesson11/body_builder.rb
@@ -0,0 +1,24 @@
+class BodyBuilder
+  def initialize(triceps, biceps, deltovidka)
+  @triceps = triceps
+  @biceps = biceps
+  @deltovidka = deltovidka
+  end
+
+  def pump(muscle)
+    case muscle
+    when 'triceps'
+      @triceps += 1
+    when 'biceps'
+      @biceps += 1
+    when 'deltovidka'
+      @deltovidka +=1
+    end
+  end
+
+  def show_muscles
+    puts "Трицепс: #{@triceps}"
+    puts "Бицепс: #{@biceps}"
+    puts "Дельтовидка: #{@deltovidka}"
+  end
+end
