0d7e0e17e3934c53efef442d3125e861d388830d
DimaYakimenko <yakimenkodims@gmail.com>
2022-06-15 20:37:11 +0300

    lesson13

---
+++ lesson13/open_file.rb
@@ -0,0 +1,15 @@
+file = File.new("./data/text.txt")
+content = file.readlines
+puts "Всего строк: #{content.size}"
+
+count = 0
+
+for item in content do
+  if item == "\n"
+    count += 1
+  end
+end
+puts "Пустых строк: #{count}"
+
+puts 'Последние 5 строк в файле:'
+puts content.last(5)
